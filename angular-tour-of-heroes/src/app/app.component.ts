7c6b4a30b5e70d9da9da8031f2d20a07f7b9d05f
lacekaa <laraspa@gmx.de>
2023-09-02 01:11:26 +0200

    the hero editor

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HeroesComponent} from "./heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  hero = { id: 1, name: 'superman' };
 }
