51dab91716943b541ceecd1f0a4b22b55d91cabb
Kassmann <lara.kassmann@ifm.com>
2023-09-22 09:51:51 +0200

    adding chapter 2 styling

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,11 +11,17 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+//  Hero: Hero = {
+//   id: 1,
+// name: 'Windstorm'
+// };
 
   heroes = HEROES;
+  protected readonly onselect = onselect;
 }
 
